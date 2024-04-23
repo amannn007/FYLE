7be3aca86b5b2085b6ab4e72f634be06d05dc37d
Aman Kumar <95896562+amannn007@users.noreply.github.com>
2024-04-23 11:55:01 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+# Submission for Fyle Frontend Challenge
+## Assumptions
+
+No extra assumptions were made. The challenge is completed according to the instructions.
+
+## Methods Implemented in API service
+There are two methods that are implemented in API service.
+
+### 1. getUser()
+This methods takes one string argument i.e. github username, and returns all the public data about user such as name, bio, location, twitter handle etc.
+
+### 2. getRepos()
+This methods takes one string argument i.e. github username and one number argument i.e. perpage (repositories to display per page. By default: 10) , and returns an array of objects in which each object contains specified number of repositories data.
+
+## Testing
+All the test cases can be run using "ng test" command.
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+
+describe('AppComponent', () => {
+
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      declarations: [AppComponent],
+    });
+
+    // Inject the http service and test controller for each test
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'fyle-frontend-challenge'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('fyle-frontend-challenge');
+  });
+
+  // Test case to check getData() method
+  it(`should return true if is request completed successfully `, ()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const result = app.getData("johnpapa");
+    expect(result).toBeTruthy();
+  })
+
+
+});
